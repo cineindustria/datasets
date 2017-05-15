bc1c349c3d5beb24fdc5b0cf9d1795d479bfe05f
Nicolás Silva <sovietfrance@gmail.com>
2017-05-14 20:24:13 -0400

    feat(Spider): Add Film schools spider

---
+++ datasets/datasets/spiders/FilmSchoolsSpider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class FilmSchoolsSpider(scrapy.Spider):
+    name = "FilmSchools"
+
+    def start_requests(self):
+        urls = [
+            'https://en.wikipedia.org/wiki/List_of_film_schools',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
